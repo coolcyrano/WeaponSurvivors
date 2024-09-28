cee55456bb3a458880b84f2e0cbbfb36958752da
coolcyrano <vanparyscyrano@gmail.com>
2024-09-28 14:53:18 +0200

    a lot of bugs added

--- Assets/cameramovement.cs
+++ Assets/cameramovement.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class cameramovement : MonoBehaviour
 {
- 
     public float sensitivity = 10f;
     public float maxYAngle = 80f;
     private Vector2 currentRotation;
     public Transform player;
+    public Camera mainCam;
+
     void Update()
     {
         currentRotation.x += Input.GetAxis("Mouse X") * sensitivity;
         currentRotation.y -= Input.GetAxis("Mouse Y") * sensitivity;
+
         currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
         currentRotation.y = Mathf.Clamp(currentRotation.y, -maxYAngle, maxYAngle);
-        Camera.main.transform.rotation = Quaternion.Euler(currentRotation.y,currentRotation.x,0);
-          player.rotation = Camera.main.transform.rotation;
+
+        player.rotation = Quaternion.Euler(currentRotation.y,currentRotation.x,0);
+
+        //player.rotation = mainCam.transform.rotation;
+
         if (Input.GetMouseButtonDown(0))
+        {
             Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 }
